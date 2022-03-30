0b1c15d3d4e1949b57ae3b7da401ca64df900d4d
hp <sahalnurdin@gmail.com>
2022-03-30 20:46:39 +0700

    Finished!

--- main.py
+++ main.py
@@ -1,169 +1,42 @@
-"Ini adalah program yang saya buat"
 
-import tkinter as tk
+import math
 
-LARGE_FONT_STYLE = ("Arial", 40, "bold")
-SMALL_FONT_STYLE = ("Arial", 16)
-DIGITS_FONT_STYLE = ("Arial", 24, "bold")
-DEFAULT_FONT_STYLE = ("Arial", 20)
+input1 = eval(input("Masukkan angka pertama: "))
+input2 = eval(input("Masukkan angka kedua: "))
 
-OFF_ABU = "#E8E8E8"             # PAPAN HITUNG
-COKLAT = "#F4F4F2"              # PAPAN NOMOR
-ABU_TERANG = "#E8E8E8"          # PAPAN HITUNG
-COKLAT_TERANG = "#EEEEEE"       # PAPAN HASIL
-LABEL_COLOR = "#495464"         # WARNA NOMOR
+print("Menu Kalkulator")
+print("Pilihlah menu berikut.")
+print("1. Penjumlahan")
+print("2. Pengurangan")
+print("3. Pembagian")
+print("4. Perkalian")
+print("5. Modulo")
+print("6. Perpangkatan")
+print("7. Akar")
+print("8. Keluar")
 
-
-class Calculator:
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.geometry("375x667")
-        self.window.resizable(0, 0)
-        self.window.title("Calculator")
-
-        self.total_expression = ""
-        self.current_expression = ""
-        self.display_frame = self.create_display_frame()
-
-        self.total_label, self.label = self.create_display_labels()
-
-        self.digits = {
-            7: (1, 1), 8: (1, 2), 9: (1, 3),
-            4: (2, 1), 5: (2, 2), 6: (2, 3),
-            1: (3, 1), 2: (3, 2), 3: (3, 3),
-            0: (4, 2), '.': (4, 1)
-        }
-        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
-        self.buttons_frame = self.create_buttons_frame()
-
-        self.buttons_frame.rowconfigure(0, weight=1)
-        for x in range(1, 5):
-            self.buttons_frame.rowconfigure(x, weight=1)
-            self.buttons_frame.columnconfigure(x, weight=1)
-        self.create_digit_buttons()
-        self.create_operator_buttons()
-        self.create_special_buttons()
-        self.bind_keys()
-
-    def bind_keys(self):
-        self.window.bind("<Return>", lambda event: self.evaluate())
-        for key in self.digits:
-            self.window.bind(str(key), lambda event, digit=key: self.add_to_expression(digit))
-
-        for key in self.operations:
-            self.window.bind(key, lambda event, operator=key: self.append_operator(operator))
-
-    def create_special_buttons(self):
-        self.create_clear_button()
-        self.create_equals_button()
-        self.create_square_button()
-        self.create_sqrt_button()
-
-    def create_display_labels(self):
-        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=COKLAT_TERANG,
-                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
-        total_label.pack(expand=True, fill='both')
-
-        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=COKLAT_TERANG,
-                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
-        label.pack(expand=True, fill='both')
-
-        return total_label, label
-
-    def create_display_frame(self):
-        frame = tk.Frame(self.window, height=221, bg=COKLAT_TERANG)
-        frame.pack(expand=True, fill="both")
-        return frame
-
-    def add_to_expression(self, value):
-        self.current_expression += str(value)
-        self.update_label()
-
-    def create_digit_buttons(self):
-        for digit, grid_value in self.digits.items():
-            button = tk.Button(self.buttons_frame, text=str(digit), bg=COKLAT, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE,
-                               borderwidth=0, command=lambda x=digit: self.add_to_expression(x))
-            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
-
-    def append_operator(self, operator):
-        self.current_expression += operator
-        self.total_expression += self.current_expression
-        self.current_expression = ""
-        self.update_total_label()
-        self.update_label()
-
-    def create_operator_buttons(self):
-        i = 0
-        for operator, symbol in self.operations.items():
-            button = tk.Button(self.buttons_frame, text=symbol, bg=OFF_ABU, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                               borderwidth=0, command=lambda x=operator: self.append_operator(x))
-            button.grid(row=i, column=4, sticky=tk.NSEW)
-            i += 1
-
-    def clear(self):
-        self.current_expression = ""
-        self.total_expression = ""
-        self.update_label()
-        self.update_total_label()
-
-    def create_clear_button(self):
-        button = tk.Button(self.buttons_frame, text="C", bg=OFF_ABU, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                           borderwidth=0, command=self.clear)
-        button.grid(row=0, column=1, sticky=tk.NSEW)
-
-    def square(self):
-        self.current_expression = str(eval(f"{self.current_expression}**2"))
-        self.update_label()
-
-    def create_square_button(self):
-        button = tk.Button(self.buttons_frame, text="x\u00b2", bg=OFF_ABU, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                           borderwidth=0, command=self.square)
-        button.grid(row=0, column=2, sticky=tk.NSEW)
-
-    def sqrt(self):
-        self.current_expression = str(eval(f"{self.current_expression}**0.5"))
-        self.update_label()
-
-    def create_sqrt_button(self):
-        button = tk.Button(self.buttons_frame, text="\u221ax", bg=OFF_ABU, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                           borderwidth=0, command=self.sqrt)
-        button.grid(row=0, column=3, sticky=tk.NSEW)
-
-    def evaluate(self):
-        self.total_expression += self.current_expression
-        self.update_total_label()
-        try:
-            self.current_expression = str(eval(self.total_expression))
-
-            self.total_expression = ""
-        except Exception as e:
-            self.current_expression = "Error"
-        finally:
-            self.update_label()
-
-    def create_equals_button(self):
-        button = tk.Button(self.buttons_frame, text="=", bg=ABU_TERANG, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
-                           borderwidth=0, command=self.evaluate)
-        button.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
-
-    def create_buttons_frame(self):
-        frame = tk.Frame(self.window)
-        frame.pack(expand=True, fill="both")
-        return frame
-
-    def update_total_label(self):
-        expression = self.total_expression
-        for operator, symbol in self.operations.items():
-            expression = expression.replace(operator, f' {symbol} ')
-        self.total_label.config(text=expression)
-
-    def update_label(self):
-        self.label.config(text=self.current_expression[:11])
-
-    def run(self):
-        self.window.mainloop()
-
-
-if __name__ == "__main__":
-    calc = Calculator()
-    calc.run()
+while True:
+    pilihan = int(input("Masukkan pilihan nomor 1/2/3/4/5/6/7/8: "))
+    if pilihan in (1, 2, 3, 4, 5, 6, 7, 8):
+        if pilihan == 1:
+            print("Hasil dari penjumlahan kedua bilangan tersebut adalah ", input1 + input2)
+        elif pilihan == 2:
+            print("Hasil dari pengurangan kedua bilangan tersebut adalah ", input1 - input2)
+        elif pilihan == 3:
+            print("Hasil dari pembagian kedua bilangan tersebut adalah ", input1 / input2)
+        elif pilihan == 4:
+            print("Hasil dari perkalian kedua bilangan tersebut adalah ", input1 * input2)
+        elif pilihan == 5:
+            print("Hasil dari modulo kedua bilangan tersebut adalah ", input1 % input2)
+        elif pilihan == 6:
+            input3 = eval(input("Masukkan pangkat yang mau dicari dari kedua bilangan: "))
+            print("Hasil dari perpangkatan kedua bilangan 1 adalah ", input1 ** input3)
+            print("Hasil dari perpangkatan kedua bilangan 2 adalah ", input2 ** input3)
+        elif pilihan == 7:
+            print("Hasil dari akar kedua bilangan 1 adalah ", math.sqrt(input1))
+            print("Hasil dari akar kedua bilangan 2 adalah ", math.sqrt(input2))
+        elif pilihan == 8:
+            print("Terima kasih :)")
+            exit()
+    else:
+        print("Maaf pilihan yang Anda input tidak ada dalam sistem, Mohon coba kembali")
